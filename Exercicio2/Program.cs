da8fd4d7523494290af55753d5691b5ccb6ad523
fcventura02 <filipecvr4@gmail.com>
2021-06-16 19:20:51 -0300

    Created function added and list

--- Exercicio2/Program.cs
+++ Exercicio2/Program.cs
@@ -1,12 +1,186 @@
 ﻿using System;
+using System.Collections.Generic;
+using Exercicio2.Entities;
 
 namespace Exercicio2
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		static int Id_Produto = 0;
+		static int Id_Venda = 0;
+		static List<Produto> produtos = new List<Produto>();
+		static List<Venda> vendas = new List<Venda>();
+		static void Main(string[] args)
+		{
+			Menu();
+		}
+		static void Menu()
+		{
+			var isRun = true;
+			var isInserted = false;
+			string opt;
+			while (isRun)
+			{
+				Console.WriteLine("------------- Menu ---------------");
+				Console.WriteLine("1 -> Adicionar produto.");
+				Console.WriteLine("2 -> Adicionar Venda. ");
+				Console.WriteLine("3 -> Listar produtos. ");
+				Console.WriteLine("4 -> Listar vendas. ");
+				Console.WriteLine("5 -> Sair. ");
+				opt = Console.ReadLine();
+				Console.Clear();
+				switch (opt)
+				{
+					case ("1"):
+						Console.WriteLine("Adicionar produto.");
+						while (!isInserted)
+						{
+							AddProduct();
+
+							Console.WriteLine("Inserir novo produto? ");
+							Console.WriteLine("Sim -> S ");
+							Console.WriteLine("Não -> Precione ENTER para continuar. ");
+							opt = Console.ReadLine();
+							if (String.IsNullOrEmpty(opt))
+							{
+								isInserted = true;
+							}
+						}
+						isInserted = false;
+						break;
+					case ("2"):
+						Console.WriteLine("Adicionar Venda.");
+						while (!isInserted)
+						{
+							if (produtos.Count == 0)
+							{
+								Console.WriteLine("Você não possui produtos para vender");
+								Console.WriteLine("");
+								Console.WriteLine("Precione ENTER para continuar....");
+								Console.ReadLine();
+								break;
+							}
+							newSale();
+
+							Console.WriteLine("Inserir nova venda? ");
+							Console.WriteLine("Sim -> S ");
+							Console.WriteLine("Não -> Precione ENTER para continuar. ");
+							opt = Console.ReadLine();
+							if (String.IsNullOrEmpty(opt))
+							{
+								isInserted = true;
+							}
+						}
+						isInserted = false;
+						break;
+					case ("3"):
+						Console.WriteLine("Listar produtos.");
+						ListarProduto(produtos);
+						Console.WriteLine("Precione ENTER para continuar....");
+						Console.ReadLine();
+						break;
+					case ("4"):
+						Console.WriteLine("Listar produtos.");
+						ListarVendas(vendas);
+						Console.WriteLine("Precione ENTER para continuar....");
+						Console.ReadLine();
+						break;
+					case ("5"):
+						isRun = false;
+						break;
+
+					default:
+						Console.WriteLine("Opção não encontrada.");
+						break;
+				}
+				Console.Clear();
+			}
+		}
+
+		static void AddProduct()
+		{
+			try
+			{
+				Console.Write("Nome: ");
+				var name = Console.ReadLine();
+				Console.Write("Preço: ");
+				var price = Convert.ToDouble(Console.ReadLine());
+				produtos.Add(new Produto(Id_Produto, name, price));
+            Id_Produto++;
+			}
+			catch (FormatException)
+			{
+				Console.Write("Você inseriu um valor invalido para o preço.");
+			}
+		}
+		static void newSale()
+		{
+			if (produtos.Count == 0)
+			{
+				Console.WriteLine("Você não possui produtos para vender");
+			}
+			else
+			{
+				var produtosVendido = new List<ProdutosVendidos>();
+				Produto produto = null;
+				var desconto = false;
+				var isInserted = false;
+				while (!isInserted)
+				{
+					try
+					{
+						Console.Write("");
+						Console.Write("Id do produto: ");
+						var id = Convert.ToInt32(Console.ReadLine());
+						Console.Write("Quantidade: ");
+						var quantidade = Convert.ToInt32(Console.ReadLine());
+						foreach (Produto item in produtos)
+						{
+							if (item.getId() == id)
+								produto = item;
+						}
+						if (produto != null)
+						{
+							produtosVendido.Add(new ProdutosVendidos(quantidade, produto));
+							Console.WriteLine("Adicionar novo produto?");
+							Console.WriteLine("Sim - S");
+							Console.WriteLine("Não - N");
+							var continuar = Console.ReadLine();
+							if (continuar.ToUpper().Contains("N")) isInserted = true;
+						}
+						else Console.WriteLine("Produto não encontrado");
+					}
+					catch (FormatException)
+					{
+						Console.Write("Você inseriu um valor invalido.");
+					}
+				}
+				if (produtosVendido.Count > 0)
+				{
+					Console.WriteLine("Adicionar desconto?");
+					Console.WriteLine("Sim - S");
+					Console.WriteLine("Não - N");
+					var adicionarDesconto = Console.ReadLine();
+					if (adicionarDesconto.ToUpper().Contains("S")) desconto = true;
+					vendas.Add(new Venda(Id_Venda, produtosVendido, desconto));
+               Id_Venda++;
+				}
+			}
+		}
+		static void ListarVendas(List<Venda> list)
+		{
+			if (list.Count == 0)
+				Console.WriteLine("Não foi possivel realizar essa operação. Por favor entre com novos valores.");
+			else foreach (Venda obj in list)
+					Console.WriteLine(obj.ToString());
+		}
+		static void ListarProduto(List<Produto> list)
+		{
+			if (list.Count == 0)
+				Console.WriteLine("Não foi possivel realizar essa operação. Por favor entre com novos valores.");
+			else foreach (Produto obj in list)
+					Console.WriteLine(obj.ToString());
+		}
+
+	}
 }
--- README.md
+++ README.md
@@ -1,24 +1,33 @@
 # Exercícios de programação:
 1.    Faça um programa que receba o nome, valor da hora trabalhada, quantidade de horas trabalhadas, quantidade de dependentes e valor do imposto a pagar de um funcionário. Calcular e exibir o salário bruto e o salário líquido desse funcionário.
 Para encontrar o salário bruto e o salário líquido:
-•    Calcular o salário base – valor da hora trabalhada * a quantidade de horas trabalhadas
-•    Para cada dependente acrescentar 30 reais
-•    Calcular o salário bruto – salário sem desconto
-•    Calcular o salário líquido – quanto que o funcionário irá receber (com desconto do valor do imposto de renda).
+•    Calcular o salário base – valor da hora trabalhada * a quantidade de horas trabalhadas;
+
+•    Para cada dependente acrescentar 30 reais;
+
+•    Calcular o salário bruto – salário sem desconto;
+
+•    Calcular o salário líquido – quanto que o funcionário irá receber (com desconto do valor do imposto de renda);
 
 2.    Faça um programa para efetuar a venda de um determinado produto. Este programa deve receber o nome do produto, a quantidade vendida e o valor unitário. Calcular e exibir:
-•    O valor total a pagar
-•    O valor total a pagar com desconto de 15%
-•    Comissão do vendedor 5%
+•    O valor total a pagar;
+
+•    O valor total a pagar com desconto de 15%;
+
+•    Comissão do vendedor 5%;
 
 3.    Sabe-se que o quilowatt de energia custa um quinto do salário mínimo. Faça um programa que receba o valor do salário mínimo e a quantidade de quilowatts gasta por uma residência. Calcule e exiba:
-•    O valor de cada quilowatt
-•    O valor a ser pago por essa residência
-•    O novo valor a ser pago por essa residência a partir de um desconto de 15%
+•    O valor de cada quilowatt;
+
+•    O valor a ser pago por essa residência;
+
+•    O novo valor a ser pago por essa residência a partir de um desconto de 15%;
+
 
 4.    Uma empresa decide dar um aumento de 30% aos funcionários cujo salário é inferior a 500 reais. Escreva um programa que receba o salário de um funcionário e exiba o valor do salário reajustado ou uma mensagem caso o funcionário não tenha direito ao aumento.
 
 5.    Faça um programa que receba a altura e o sexo de uma pessoa, calcule e exiba o seu peso ideal, utilizando as seguintes fórmulas:
 
-•    Para homens: (72.7 * altura) – 58
-•    Para mulheres: (62.1 * altura) – 44.7
+•    Para homens: (72.7 * altura) – 58;
+
+•    Para mulheres: (62.1 * altura) – 44.7;
